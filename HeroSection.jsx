30562117d2a43c4dc11c6491c4233e43cb6ded98
Premchand Reddy <132259855+pabitrareddy@users.noreply.github.com>
2023-11-22 07:57:22 +0530

    premchand

---
+++ HeroSection.jsx
@@ -0,0 +1,18 @@
+const HeroSection = () => {
+    return (
+      <main className="hero container">
+        <div className="hero-content">
+          <h1>Explore new subjects that interest you</h1>
+          <p>
+          Remember,<br /> subject registration is an important part of your educational journey
+          , and it can lead to many exciting opportunities <br />and experiences.<br />Approach it with a positive mindset <br />and a commitment to your goals.
+          </p>
+        </div>
+        <div className="hero-image">
+          <img src="images/student_PNG62536.png" alt="student_image"/>
+        </div>
+      </main>
+    );
+  };
+  
+  export default HeroSection;
